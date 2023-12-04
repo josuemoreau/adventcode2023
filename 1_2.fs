933531b6738cd480a930bc2743a7cf1b8125840b
Josué Moreau <jsmr14.12@gmail.com>
2023-12-04 23:44:51 +0100

    solution of second part of problem 1

--- 1_2.fs
+++ 1_2.fs
@@ -9,21 +9,18 @@
     \ .s CR
     drop over I + C@ over I + C@ = IF 0 ELSE 1 LEAVE THEN
   loop
-  IF -2
-  ELSE 2drop 2dup = IF 0 ELSE < IF 1 ELSE -1 THEN THEN
+  IF -2 nip nip nip nip
+  ELSE 2drop 2dup = IF 2drop 0 ELSE < IF 1 ELSE -1 THEN THEN
   THEN
 ;
 
-\ S" abcdef" S" abc" compare . CR bye
-
 : is-digit 
   over c@ dup 10 =
-  IF ( first character is \n ) -1 -2
+  IF ( first character is \n ) drop -1 -2
   ELSE dup 48 >= over 57 <= and
        IF 48 - 1
-       ELSE drop 2dup S" zero" compare abs 1 = IF 0 3
-       ELSE 2dup S" one" compare abs 1 = IF 1 2
-       ELSE 2dup S" two" compare abs 1 = IF 2 3
+       ELSE drop 2dup S" one" compare abs 1 = IF 1 2
+       ELSE 2dup S" two" compare abs 1 = IF 2 2
        ELSE 2dup S" three" compare abs 1 = IF 3 4
        ELSE 2dup S" four" compare abs 1 = IF 4 4
        ELSE 2dup S" five" compare abs 1 = IF 5 3
@@ -32,61 +29,36 @@
        ELSE 2dup S" eight" compare abs 1 = IF 8 4
        ELSE 2dup S" nine" compare abs 1 = IF 9 3
        ELSE 1 -1
-       THEN THEN THEN THEN THEN THEN THEN THEN THEN THEN THEN
+       THEN THEN THEN THEN THEN THEN THEN THEN THEN THEN
   THEN
+  2swap drop drop
 ;
-
-: test
-  S" twoblablabla" .s CR is-digit .s CR case
-  -2 of drop ." end of line -- +1" endof
-  -1 of drop ." not found -- +1" endof
-  swap ." digit " . dup ."  -- " .
-  endcase CR .s CR
-;
-
-test
-bye
 
 variable sum
 
+variable break 200000 break !
+
+: print-if-break-gt-0 
+break @ if dup dup . ." -> " sum @ + . CR break @ 1- break ! else then
+;
+
 : parse
-  CR
   0 0
   BEGIN 2 pick 0> WHILE
-    3 pick 3 pick .s CR is-digit .s CR case
-    -2 of 0 2swap swap 10 * + sum @ + sum ! 2rot 1- swap 1+ swap 2rot 2rot nip 0 0 rot endof
-    -1 of 0 .s CR 2rot bye 3 pick - swap 3 pick + swap 2rot 2rot nip endof
-    2swap dup 0= IF drop 2 pick ELSE THEN 2swap
-    2rot 3 pick - swap 3 pick + swap 2rot 2rot nip
+    3 pick 3 pick is-digit case
+    -2 of 0 2swap swap 10 * + print-if-break-gt-0 sum @ + sum ! 2drop 1- swap 1+ swap 0 0 endof
+    -1 of 0 2rot 1- swap 1+ swap 2rot 2rot 2drop endof
+    2swap drop 2 pick swap dup 0= IF drop 2 pick ELSE THEN swap 2swap
+    2rot 2 pick - swap 2 pick + swap 2rot 2rot nip
     endcase
   REPEAT
 ;
 
-\ : first-last-digit
-\   0 0 0 3 pick 0
-\   DO     drop 3 pick i + 3 pick i - . CR c@ dup 10 = 
-\          IF     drop i 1+ LEAVE 
-\          ELSE   
-\          THEN 
-\          dup 48 >= over 57 <= and 
-\          IF     2 pick 0= 
-\                 IF     rot drop dup rot rot 
-\                 ELSE   
-\                 THEN 
-\                 nip 
-\          ELSE   drop 
-\          THEN 
-\          i' 
-\   LOOP
-\   ;
-\ : parse-line first-last-digit 2rot 2 pick + 2rot swap 4 pick - swap 2rot drop 48 - swap 48 - 10 * + ;
-\ : advcodeloop1_1  
-\   parse-line sum @ + sum ! dup 0> 
-\   IF     recurse
-\   ELSE   
-\   THEN ;
-: advcode1_1  
-  0 sum ! parse ;
+\ s\" two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n"
+\ 0 sum ! parse sum @ CR . CR bye
+
+: advcode1_1
+  0 sum ! CR parse ;
 create buffer 1000000 allot
 : open-read R/O open-file drop dup dup file-size drop drop buffer swap rot read-file drop swap close-file drop ;
 0 S" input1_1" open-read buffer swap advcode1_1 sum @ CR . CR bye
