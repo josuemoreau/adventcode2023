0287c7e92f9d1543984c86a34927c9d6cabad29f
Josué Moreau <jsmr14.12@gmail.com>
2023-12-04 15:46:22 +0100

    solution for problem 1

--- 1_1.fs
+++ 1_1.fs
@@ -1,4 +1,4 @@
-: test  
+: first-last-digit
   0 0 0 3 pick 0 
   DO     drop 3 pick i + c@ dup 10 = 
          IF     drop i 1+ LEAVE 
@@ -15,27 +15,15 @@
          i' 
   LOOP
   ;
-: test1 test 2rot 2 pick + 2rot swap 4 pick - swap 2rot drop 48 - swap 48 - 10 * + ;
-: test2  
-  dup 0 
-  DO     i 1+ pick i + c@ 
-  LOOP
-  ;
-\ : free  
-\   depth 0> 
-\   IF     depth 0 
-\          DO     drop 
-\          LOOP
-         
-\   ELSE   
-\   THEN ;
+: parse-line first-last-digit 2rot 2 pick + 2rot swap 4 pick - swap 2rot drop 48 - swap 48 - 10 * + ;
 variable sum
 : advcodeloop1_1  
-  test1 sum @ + sum ! dup 0> 
+  parse-line sum @ + sum ! dup 0> 
   IF     recurse
   ELSE   
   THEN ;
 : advcode1_1  
   0 sum ! advcodeloop1_1 ;
-0 s\" 1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet" advcode1_1 sum @ CR . CR
-bye
+create buffer 1000000 allot
+: open-read R/O open-file drop dup dup file-size drop drop buffer swap rot read-file drop swap close-file drop ;
+0 S" input1_1" open-read buffer swap advcode1_1 sum @ CR . CR bye
