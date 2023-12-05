8e6a848e1a900efac3622f318404d3d0f1165231
Josué Moreau <jsmr14.12@gmail.com>
2023-12-05 23:55:49 +0100

    solution for part 1 of problem 3

--- 3_1.fs
+++ 3_1.fs
@@ -17,7 +17,7 @@ variable sum
   then
 
   0= if
-    addr2 addr1 - pos + len @ mod if
+    addr2 addr1 - pos 1+ + len @ mod if
         addr2 C@ '.' <> addr2 len @ - C@ '.' <> or addr2 len @ + C@ '.' <> or
     else 0
     then
@@ -32,7 +32,7 @@ variable sum
 
 : read-number ( addr1 u1 n1 -- addr2 u2 n2 n ) { addr1 u1 n1 -- addr2 u2 n2 n }
   0 0 addr1 u1 >number ( u u addr2 u2 )
-  over addr1 swap n1 ( u u addr2 u2 addr1 addr2 n1 ) check-number CR .s
+  over addr1 swap n1 ( u u addr2 u2 addr1 addr2 n1 ) check-number
   if 2swap drop over u1 swap - n1 + swap
   else 2nip dup u1 swap - n1 + -1
   then
@@ -41,16 +41,42 @@ variable sum
 : parse ( addr u -- )
   0 begin over 0> while
     2 pick C@ dup dup 48 >= swap 57 <= and if
-      drop 1+ rot 1+ rot 1- rot
+      drop read-number dup 0> if
+        ." Found " dup .
+        sum @ + sum !
+        ."  -> " sum @ . .s CR
+      else drop
+      then
     else 10 = if 
       drop 0 rot 1+ rot 1- rot
     else
       1+ rot 1+ rot 1- rot
     then then
   repeat
-\   2drop drop
+  2drop drop
 ;
 
-\ create buffer 1000000 allot
-6 len !
-s\" .....\n.123.\n....." 7 - swap 7 + swap 1 read-number CR .s CR bye
+create buffer 1000000 allot
+
+: get-line-length ( x name -- x n )
+  R/O open-file drop dup dup file-size drop drop buffer swap rot read-line 2drop swap close-file drop ; 
+
+: fill ( buf c len -- )
+  0 do dup 2 pick I + ! loop 2drop ;
+
+: open-read ( x name u buf -- x n ) { name u buf }
+  name u R/O open-file drop dup dup file-size drop drop buf swap rot read-file drop swap close-file drop
+;
+
+: advcode 0 sum ! 
+  2dup get-line-length len ! \ get line length
+  buffer '.' len @ fill \ fill a first line with dots
+  len @ buffer + dup 10 swap ! 1+ \ add \n and shift pointer to buffer
+  rot rot 2 pick open-read \ read the file
+  swap over + '.' len @ fill \ fill last line with dots
+  len @ 1+ len ! \ increment len because of \n at end of lines
+  CR buffer swap len @ 2* +
+  parse sum @
+;
+
+0 S" input3" advcode . CR bye
